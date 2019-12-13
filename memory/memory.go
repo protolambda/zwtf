79dd04aedfb7b45160273189399a5020b9ed9367
protolambda <proto@protolambda.com>
2019-12-13 18:58:01 +0100

    request states and cache committees

--- memory/memory.go
+++ memory/memory.go
@@ -1,7 +1,12 @@
 package memory
 
 import (
+	"errors"
+	"fmt"
+	"github.com/protolambda/zrnt/eth2/beacon/seeding"
+	"github.com/protolambda/zrnt/eth2/beacon/shuffling"
 	. "github.com/protolambda/zrnt/eth2/core"
+	"github.com/protolambda/zrnt/eth2/phase0"
 	"zwtf/events"
 )
 
@@ -22,10 +27,11 @@ type BlockSummary struct {
 }
 
 type AttestationSummary struct {
-	Slot   Slot
-	Head   BlockPtr
-	Target BlockPtr
-	Source BlockPtr
+	Slot      Slot
+	CommIndex CommitteeIndex
+	Head      BlockPtr
+	Target    BlockPtr
+	Source    BlockPtr
 }
 
 type VoteSummary struct {
@@ -52,36 +58,42 @@ type Memory struct {
 }
 
 type MemoryUpdater struct {
-	LastMemoryDump MemoryState
-	CurrentMemory  Memory
-	Blocks         map[Root]BlockPtr
-	Votes          map[ValidatorIndex]LatestVotesPtr
+	lastMemoryDump       MemoryState
+	currentMemory        Memory
+	blocks               map[Root]BlockPtr
+	votes                map[ValidatorIndex]LatestVotesPtr
+	epochCommitteesCache map[Root][SLOTS_PER_EPOCH][MAX_COMMITTEES_PER_SLOT][]ValidatorIndex
+	finalizedState       *phase0.BeaconState
+	headState            *phase0.BeaconState
 }
 
 func (m *MemoryUpdater) PruneBlocks() {
-	pruneBlockPtr := m.CurrentMemory.BlocksNextPtr
+	pruneBlockPtr := m.currentMemory.BlocksNextPtr
 	if pruneBlockPtr < BlocksMemory {
 		// nothing to prune
 		return
 	}
 	pruneBlockPtr -= BlocksMemory
-	for r, blockPtr := range m.Blocks {
+	for r, blockPtr := range m.blocks {
 		if blockPtr < pruneBlockPtr {
-			delete(m.Blocks, r)
+			delete(m.blocks, r)
+			// also prune epoch-committees corresponding to these blocks
+			// if accidentally pruning more recent data (end of epoch still within view), then we can always refetch this data.
+			delete(m.epochCommitteesCache, r)
 		}
 	}
 }
 
 func (m *MemoryUpdater) PruneVotes() {
-	pruneLatestVotePtr := m.CurrentMemory.LatestVotesNextPtr
+	pruneLatestVotePtr := m.currentMemory.LatestVotesNextPtr
 	if pruneLatestVotePtr < LatestVotesMemory {
 		// nothing to prune
 		return
 	}
 	pruneLatestVotePtr -= LatestVotesMemory
-	for vi, votePtr := range m.Votes {
+	for vi, votePtr := range m.votes {
 		if votePtr < pruneLatestVotePtr {
-			delete(m.Votes, vi)
+			delete(m.votes, vi)
 		}
 	}
 }
@@ -97,16 +109,16 @@ type MemoryDiff struct {
 }
 
 func (m *MemoryUpdater) BuildDiff() *MemoryDiff {
-	pre := m.LastMemoryDump
-	now := m.CurrentMemory.MemoryState
+	pre := m.lastMemoryDump
+	now := m.currentMemory.MemoryState
 	out := &MemoryDiff{
-		Previous: pre,
-		Current: now,
-		Head: make([]BlockPtr, 0, now.HeadNextPtr - pre.HeadNextPtr),
-		Finalized: make([]BlockPtr, 0, now.FinalizedNextPtr - pre.FinalizedNextPtr),
-		Blocks: make([]BlockSummary, 0, now.BlocksNextPtr - pre.BlocksNextPtr),
-		Attestations: make([]AttestationSummary, 0, now.AttestationsNextPtr - pre.AttestationsNextPtr),
-		LatestVotes: make([]VoteSummary, 0, now.LatestVotesNextPtr - pre.LatestVotesNextPtr),
+		Previous:     pre,
+		Current:      now,
+		Head:         make([]BlockPtr, 0, now.HeadNextPtr-pre.HeadNextPtr),
+		Finalized:    make([]BlockPtr, 0, now.FinalizedNextPtr-pre.FinalizedNextPtr),
+		Blocks:       make([]BlockSummary, 0, now.BlocksNextPtr-pre.BlocksNextPtr),
+		Attestations: make([]AttestationSummary, 0, now.AttestationsNextPtr-pre.AttestationsNextPtr),
+		LatestVotes:  make([]VoteSummary, 0, now.LatestVotesNextPtr-pre.LatestVotesNextPtr),
 	}
 	// No generics, and easier hardcoded than making MemoryDiff more abstract.
 	// for each buffer: either the diff wraps around, or not
@@ -114,50 +126,50 @@ func (m *MemoryUpdater) BuildDiff() *MemoryDiff {
 		a := pre.HeadNextPtr % HeadsMemory
 		b := now.HeadNextPtr % HeadsMemory
 		if b < a {
-			out.Head = append(out.Head, m.CurrentMemory.HeadBuffer[a:0]...)
-			out.Head = append(out.Head, m.CurrentMemory.HeadBuffer[0:b]...)
+			out.Head = append(out.Head, m.currentMemory.HeadBuffer[a:0]...)
+			out.Head = append(out.Head, m.currentMemory.HeadBuffer[0:b]...)
 		} else {
-			out.Head = append(out.Head, m.CurrentMemory.HeadBuffer[a:b]...)
+			out.Head = append(out.Head, m.currentMemory.HeadBuffer[a:b]...)
 		}
 	}
 	{
 		a := pre.FinalizedNextPtr % FinalizedMemory
 		b := now.FinalizedNextPtr % FinalizedMemory
 		if b < a {
-			out.Finalized = append(out.Finalized, m.CurrentMemory.FinalizedBuffer[a:0]...)
-			out.Finalized = append(out.Finalized, m.CurrentMemory.FinalizedBuffer[0:b]...)
+			out.Finalized = append(out.Finalized, m.currentMemory.FinalizedBuffer[a:0]...)
+			out.Finalized = append(out.Finalized, m.currentMemory.FinalizedBuffer[0:b]...)
 		} else {
-			out.Finalized = append(out.Finalized, m.CurrentMemory.FinalizedBuffer[a:b]...)
+			out.Finalized = append(out.Finalized, m.currentMemory.FinalizedBuffer[a:b]...)
 		}
 	}
 	{
 		a := pre.BlocksNextPtr % BlocksMemory
 		b := now.BlocksNextPtr % BlocksMemory
 		if b < a {
-			out.Blocks = append(out.Blocks, m.CurrentMemory.BlocksBuffer[a:0]...)
-			out.Blocks = append(out.Blocks, m.CurrentMemory.BlocksBuffer[0:b]...)
+			out.Blocks = append(out.Blocks, m.currentMemory.BlocksBuffer[a:0]...)
+			out.Blocks = append(out.Blocks, m.currentMemory.BlocksBuffer[0:b]...)
 		} else {
-			out.Blocks = append(out.Blocks, m.CurrentMemory.BlocksBuffer[a:b]...)
+			out.Blocks = append(out.Blocks, m.currentMemory.BlocksBuffer[a:b]...)
 		}
 	}
 	{
 		a := pre.AttestationsNextPtr % AttestationsMemory
 		b := now.AttestationsNextPtr % AttestationsMemory
 		if b < a {
-			out.Attestations = append(out.Attestations, m.CurrentMemory.AttestationsBuffer[a:0]...)
-			out.Attestations = append(out.Attestations, m.CurrentMemory.AttestationsBuffer[0:b]...)
+			out.Attestations = append(out.Attestations, m.currentMemory.AttestationsBuffer[a:0]...)
+			out.Attestations = append(out.Attestations, m.currentMemory.AttestationsBuffer[0:b]...)
 		} else {
-			out.Attestations = append(out.Attestations, m.CurrentMemory.AttestationsBuffer[a:b]...)
+			out.Attestations = append(out.Attestations, m.currentMemory.AttestationsBuffer[a:b]...)
 		}
 	}
 	{
 		a := pre.LatestVotesNextPtr % LatestVotesMemory
 		b := now.LatestVotesNextPtr % LatestVotesMemory
 		if b < a {
-			out.LatestVotes = append(out.LatestVotes, m.CurrentMemory.LatestVotesBuffer[a:0]...)
-			out.LatestVotes = append(out.LatestVotes, m.CurrentMemory.LatestVotesBuffer[0:b]...)
+			out.LatestVotes = append(out.LatestVotes, m.currentMemory.LatestVotesBuffer[a:0]...)
+			out.LatestVotes = append(out.LatestVotes, m.currentMemory.LatestVotesBuffer[0:b]...)
 		} else {
-			out.LatestVotes = append(out.LatestVotes, m.CurrentMemory.LatestVotesBuffer[a:b]...)
+			out.LatestVotes = append(out.LatestVotes, m.currentMemory.LatestVotesBuffer[a:b]...)
 		}
 	}
 	return out
@@ -181,45 +193,45 @@ func (m *MemoryUpdater) OnEvent(ev *events.Event) {
 }
 
 func (m *MemoryUpdater) OnBlockIdentity(root Root, slot Slot, parent Root) BlockPtr {
-	if i, ok := m.Blocks[root]; ok {
+	if i, ok := m.blocks[root]; ok {
 		// backfill summary data
-		if summary := &m.CurrentMemory.BlocksBuffer[i%BlocksMemory]; summary.Slot == 0 {
-			summary.Parent = m.Blocks[parent]
+		if summary := &m.currentMemory.BlocksBuffer[i%BlocksMemory]; summary.Slot == 0 {
+			summary.Parent = m.blocks[parent]
 			summary.Slot = slot
 		}
 		return i
 	} else {
-		i = m.CurrentMemory.BlocksNextPtr
-		m.Blocks[root] = i
-		m.CurrentMemory.BlocksBuffer[i%BlocksMemory] = BlockSummary{
+		i = m.currentMemory.BlocksNextPtr
+		m.blocks[root] = i
+		m.currentMemory.BlocksBuffer[i%BlocksMemory] = BlockSummary{
 			Slot:   slot,
 			HTR:    root,
-			Parent: m.Blocks[parent], // may still be 0 if parent is unknown
+			Parent: m.blocks[parent], // may still be 0 if parent is unknown
 		}
-		m.CurrentMemory.BlocksNextPtr = i + 1
+		m.currentMemory.BlocksNextPtr = i + 1
 		return i
 	}
 }
 
 func (m *MemoryUpdater) GetAttestation(attPtr AttestationPtr) *AttestationSummary {
-	if attPtr+AttestationsMemory <= m.CurrentMemory.AttestationsNextPtr {
+	if attPtr+AttestationsMemory <= m.currentMemory.AttestationsNextPtr {
 		// previous attestation is so outdated that it's not in memory anymore
 		return nil
 	}
-	return &m.CurrentMemory.AttestationsBuffer[attPtr&AttestationsMemory]
+	return &m.currentMemory.AttestationsBuffer[attPtr&AttestationsMemory]
 }
 
 func (m *MemoryUpdater) IsOutdatedVote(index ValidatorIndex, attPtr AttestationPtr) bool {
-	prevPtr, ok := m.Votes[index]
+	prevPtr, ok := m.votes[index]
 	if !ok {
 		// no previous vote
 		return true
 	}
-	if prevPtr+LatestVotesMemory <= m.CurrentMemory.LatestVotesNextPtr {
+	if prevPtr+LatestVotesMemory <= m.currentMemory.LatestVotesNextPtr {
 		// previous vote is so outdated that it's not in memory anymore
 		return true
 	}
-	prevVote := m.CurrentMemory.LatestVotesBuffer[prevPtr%LatestVotesMemory]
+	prevVote := m.currentMemory.LatestVotesBuffer[prevPtr%LatestVotesMemory]
 	prevAtt := m.GetAttestation(prevVote.AttestationPtr)
 	if prevAtt == nil {
 		return true
@@ -233,15 +245,15 @@ func (m *MemoryUpdater) IsOutdatedVote(index ValidatorIndex, attPtr AttestationP
 
 func (m *MemoryUpdater) OnVoteIdentity(index ValidatorIndex, attPtr AttestationPtr) LatestVotesPtr {
 	if !m.IsOutdatedVote(index, attPtr) {
-		return m.Votes[index]
+		return m.votes[index]
 	} else {
-		i := m.CurrentMemory.LatestVotesNextPtr
-		m.Votes[index] = i
-		m.CurrentMemory.LatestVotesBuffer[i%LatestVotesMemory] = VoteSummary{
+		i := m.currentMemory.LatestVotesNextPtr
+		m.votes[index] = i
+		m.currentMemory.LatestVotesBuffer[i%LatestVotesMemory] = VoteSummary{
 			ValidatorIndex: index,
 			AttestationPtr: attPtr,
 		}
-		m.CurrentMemory.LatestVotesNextPtr = i + 1
+		m.currentMemory.LatestVotesNextPtr = i + 1
 		return i
 	}
 }
@@ -255,32 +267,85 @@ func (m *MemoryUpdater) OnRejectBlock(data *events.BeaconBlockRejected) {
 	// TODO handle rejected blocks?
 }
 
-//func (m *MemoryUpdater) GetAncestor(block Root, slot Slot) (Root, error) {
-//	i := m.Blocks[block]
-//	if i+BlocksMemory >= m.CurrentMemory.BlocksNextPtr {
-//
-//	}
-//	b := &m.CurrentMemory.BlocksBuffer[i%BlocksMemory]
-//}
-
-func (m *MemoryUpdater) GetCommittee(block Root) ([]ValidatorIndex, error) {
-	// TODO
+func (m *MemoryUpdater) GetState(block Root) (*phase0.BeaconState, error) {
+	// TODO http request to fetch SSZ state, deserialize it
 	return nil, nil
+}
+
+// Used to get an earlier block, but after a given slot, as anchor point to get committee data with
+func (m *MemoryUpdater) GetAncestorAtOrAfter(block Root, slot Slot) (Root, error) {
+	prevRoot := block
+	i := m.blocks[block]
+	for {
+		if i+BlocksMemory >= m.currentMemory.BlocksNextPtr {
+			return Root{}, errors.New("ancestor too old, cannot find it in buffer")
+		}
+		b := &m.currentMemory.BlocksBuffer[i%BlocksMemory]
+		if b.Slot == slot {
+			return b.HTR, nil
+		}
+		if b.Slot < slot {
+			return prevRoot, nil
+		}
+		i = b.Parent
+		prevRoot = b.HTR
+	}
+}
+
+// Subset of zrnt features to compute committee data for a whole epoch with
+type CommitteeCompute struct {
+	*phase0.BeaconState
+	shuffling.ShufflingFeature
+	seeding.SeedFeature
+}
+
+func NewCommitteeCompute(state *phase0.BeaconState) *CommitteeCompute {
+	cc := new(CommitteeCompute)
+	cc.BeaconState = state
+	cc.ShufflingFeature.Meta = cc
+	cc.SeedFeature.Meta = cc
+	return cc
+}
+
+func (m *MemoryUpdater) GetCommittee(block Root, slot Slot, commIndex CommitteeIndex) ([]ValidatorIndex, error) {
+	// try to get the first block that has access to this same committee data
+	committeeAnchorBlock, err := m.GetAncestorAtOrAfter(block, slot.ToEpoch().Previous().GetStartSlot())
+	if err != nil {
+		committeeAnchorBlock = block
+	}
+	// check if we cached it
+	if cached, ok := m.epochCommitteesCache[committeeAnchorBlock]; ok {
+		return cached[slot-slot.ToEpoch().GetStartSlot()][commIndex], nil
+	}
+	// if not in the cache, then get the corresponding state, and fetch the data
+	state, err := m.GetState(committeeAnchorBlock)
+	if err != nil {
+		return nil, fmt.Errorf("cannot get state to compute committees from, err: %v", err)
+	}
+	// compute the committees for the whole epoch
+	committeeCompute := NewCommitteeCompute(state)
+	shufEpoch := committeeCompute.LoadShufflingEpoch(slot.ToEpoch())
+	m.epochCommitteesCache[committeeAnchorBlock] = shufEpoch.Committees
+	return shufEpoch.Committees[slot-slot.ToEpoch().GetStartSlot()][commIndex], nil
 }
 
 func (m *MemoryUpdater) OnImportAttestation(data *events.BeaconAttestationImported) {
 	s := m.OnBlockIdentity(data.Attestation.Data.Source.Root, data.Attestation.Data.Source.Epoch.GetStartSlot(), Root{})
 	t := m.OnBlockIdentity(data.Attestation.Data.Target.Root, data.Attestation.Data.Target.Epoch.GetStartSlot(), Root{})
 	h := m.OnBlockIdentity(data.Attestation.Data.BeaconBlockRoot, data.Attestation.Data.Slot, Root{})
-	attPtr := m.CurrentMemory.AttestationsNextPtr
-	m.CurrentMemory.AttestationsBuffer[attPtr%AttestationsMemory] = AttestationSummary{
-		Slot:   data.Attestation.Data.Slot,
-		Head:   h,
-		Target: t,
-		Source: s,
+	attPtr := m.currentMemory.AttestationsNextPtr
+	m.currentMemory.AttestationsBuffer[attPtr%AttestationsMemory] = AttestationSummary{
+		Slot:      data.Attestation.Data.Slot,
+		CommIndex: data.Attestation.Data.Index,
+		Head:      h,
+		Target:    t,
+		Source:    s,
 	}
-	m.CurrentMemory.AttestationsNextPtr++
-	committee, err := m.GetCommittee(data.Attestation.Data.BeaconBlockRoot)
+	m.currentMemory.AttestationsNextPtr++
+	committee, err := m.GetCommittee(
+		data.Attestation.Data.BeaconBlockRoot,
+		data.Attestation.Data.Slot,
+		data.Attestation.Data.Index)
 	if err != nil {
 		return
 	}
@@ -300,12 +365,16 @@ func (m *MemoryUpdater) OnRejectAttestation(data *events.BeaconAttestationReject
 
 func (m *MemoryUpdater) OnHeadChange(data *events.BeaconHeadChanged) {
 	i := m.OnBlockIdentity(data.CurrentHeadBeaconBlockRoot, 0, Root{})
-	m.CurrentMemory.HeadBuffer[m.CurrentMemory.HeadNextPtr%HeadsMemory] = i
-	m.CurrentMemory.HeadNextPtr = i
+	m.currentMemory.HeadBuffer[m.currentMemory.HeadNextPtr%HeadsMemory] = i
+	m.currentMemory.HeadNextPtr = i
+
+	// TODO get new head state, diff with previous state, and buffer balance changes
 }
 
 func (m *MemoryUpdater) OnFinalize(data *events.BeaconFinalization) {
 	i := m.OnBlockIdentity(data.Root, 0, Root{})
-	m.CurrentMemory.FinalizedBuffer[m.CurrentMemory.FinalizedNextPtr%FinalizedMemory] = i
-	m.CurrentMemory.FinalizedNextPtr = i
+	m.currentMemory.FinalizedBuffer[m.currentMemory.FinalizedNextPtr%FinalizedMemory] = i
+	m.currentMemory.FinalizedNextPtr = i
+
+	// TODO get new finalized state
 }
